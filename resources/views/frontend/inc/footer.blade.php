09f2f91cc50a2f943b2b43af9f2a58c8e8783a49
Manojgaire1 <gairemanoj2020@gmail.com>
2021-08-25 16:05:47 +0545

    completed the template fiting and all

---
+++ resources/views/frontend/inc/footer.blade.php
@@ -0,0 +1,77 @@
+</div>
+
+<div id="footer-wrap">
+
+    <!-- CONTACT INFO  -->
+    <div id="contact2">
+        <div class="container">
+            <div class="row withMap">
+                <div class="col-12 col-md-8">
+                    <div id="googleMap"></div>
+                </div>
+                <div class="col-12 col-md-4">
+                    <div>
+                        <i class="fa fa-map-marker"> </i>223 John Adam Street, London, UK.
+                    </div>
+                    <div>
+                        <i class="fa fa-phone"> </i>800 123-456-789
+                    </div>
+                    <div>
+                        <i class="fa fa-envelope"> </i>your@email.com
+                    </div>
+                    <div class="mapinfo">
+                        <p>Donec quis tincidunt nisl. Mauris ac urna libero. Aliquam cursus, augue eu efficitur dignissim,
+                            risus mauris porta diam, in bibendum lectus enim vitae risus. Maecenas sit amet pretium quam, in
+                            tristique ante. Sed consequat rutrum metus.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- CONTACT INFO END -->
+
+    <!-- FOOTER -->
+    <footer class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-sm-6">
+                    <p>Copyright &copy; {{date('Y')}} Dr. Devi Bahadur Basnet</p>
+                </div>
+                <div class="col-12 col-sm-6 social">
+                    <p class="pull-right">
+                        <a href="#"><i class="fa fa-facebook"></i></a>
+                        <a href="#"><i class="fa fa-google"></i></a>
+                        <a href="#"><i class="fa fa-twitter"></i></a>
+                        <a href="#"><i class="fa fa-tumblr"></i></a>
+                        <a href="#"><i class="fa fa-instagram"></i></a>
+                        <a href="#"><i class="fa fa-youtube"></i></a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- FOOTER END -->
+
+</div>
+
+<!-- jQuery first, then Tether, then Bootstrap JS. -->
+<script src="{{asset('front-assets/js/jquery-3.1.1.min.js')}}"></script>
+<script src="{{asset('front-assets/js/jquery-migrate-3.0.0.min.js')}}"></script>
+<script src="{{asset('front-assets/js/tether.min.js')}}"></script>
+<script src="{{asset('front-assets/js/bootstrap.min.js')}}"></script>
+<script src="{{asset('front-assets/js/jquery.stellar.min.js')}}"></script>
+<script src="{{asset('front-assets/js/jquery.prettyPhoto.js')}}"></script>
+<script src="{{asset('front-assets/js/flickity.pkgd.min.js')}}"></script>
+<script src="{{asset('front-assets/js/isotope.pkgd.min.js')}}"></script>
+<script src="{{asset('front-assets/js/jquery.debouncedresize.js')}}"></script>
+<script src="{{asset('front-assets/js/jquery.waypoints.min.js')}}"></script>
+<script src="{{asset('front-assets/js/typed.min.js')}}"></script>
+<script src="{{asset('front-assets/js/particles.min.js')}}"></script>
+<script src="{{asset('front-assets/js/config.js')}}"></script>
+<script src="{{asset('front-assets/js/custom.js')}}"></script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCMYUCUdT1WkZJFkrpI4IzEObG8_YNcZPg&callback=myMap"></script>
+@yield('page_specific_js')
+
+</body>
+</html>
--- resources/views/frontend/inc/header.blade.php
+++ resources/views/frontend/inc/header.blade.php
@@ -1,1 +1,72 @@
-@yield('page_specific_css')
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come after these tags -->
+
+    <!-- Fill in the following meta tags -->
+    <meta name="keywords" content="keyword1, keyword2, keyword3"/>
+    <meta name="description" content="Description">
+    <meta name="author" content="Author">
+
+    <!-- Change the site title -->
+    <title>BRAX | Responsive Personal Portfolio Template</title>
+
+    <!-- You can regenerate the favicons as you wish using http://realfavicongenerator.net/ -->
+    <link rel="apple-touch-icon" sizes="180x180" href="{{asset('front-assets/favicon/apple-touch-icon.png')}}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{asset('front-assets/favicon/favicon-32x32.png')}}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{asset('front-assets/favicon/favicon-16x16.png')}}">
+    <link rel="manifest" href="{{asset('front-assets/favicon/site.webmanifest')}}">
+    <link rel="mask-icon" href="{{asset('front-assets/favicon/safari-pinned-tab.svg')}}" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="{{asset('front-assets/css/bootstrap.min.css')}}">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:200,300,300i,400,700" rel="stylesheet">
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="font-awesome-4.7.0/css/font-awesome.min.css">
+
+    <!-- Pretty Photo -->
+    <link rel="stylesheet" href="{{asset('front-assets/css/prettyPhoto.css')}}"/>
+
+    <!-- Flickity -->
+    <link rel="stylesheet" href="{{asset('front-assets/css/flickity.css')}}"/>
+
+    <!-- animate.css -->
+    <link rel="stylesheet" href="{{asset('front-assets/css/animate.css')}}"/>
+
+    <!-- custom.css - the styling for this template -->
+    <link rel="stylesheet" href="{{asset('front-assets/css/custom.css')}}">
+    <!-- load page specific css---->
+    @yield('page_specific_css')
+    <!-- Modernizr -->
+    <script src="{{asset('front-assets/js/modernizr-custom.js')}}"></script>
+</head>
+
+<body>
+
+<!-- status spinner showing while loading the page -->
+<div id="preloader"></div>
+
+<!-- HEADER -->
+<header>
+    <nav id="mySidenav" class="sidenav navbar-nav">
+        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+        <a class="nav-link" href="#hero">Home</a>
+        <a class="nav-link" href="#about">About</a>
+        <a class="nav-link" href="#services">Services</a>
+        <a class="nav-link" href="#portfolio">Portfolio</a>
+        <a class="nav-link" href="#contact">Contact</a>
+    </nav>
+    <span id="openNav" onclick="openNav()">&#9776;</span>
+</header>
+
+<!-- HEADER END -->
+<div id="content-wrap">
