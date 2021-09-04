0b170565d7794567aae40196812b8b4dd2a57ad2
Manojgaire1 <gairemanoj2020@gmail.com>
2021-09-04 14:15:35 +0545

    blog updated

--- resources/views/frontend/inc/blog.blade.php
+++ resources/views/frontend/inc/blog.blade.php
@@ -8,13 +8,13 @@
         <div class="container">
             <div class="row">
                 <div class="col-12 col-lg-4 item">
-                    <img src="{{asset('front-assets/img/blog/blog1.png')}}" class="img-fluid" alt=""/>
+                    <img src="{{asset('front-assets/img/blog/blog1.png')}}" class="img-fluid" alt="role-aim-and-upcoming-planning-of-nran"/>
                     <h4>Role, aim and upcoming planning of NRNA</h4>
 
                     <p>
                        My opinion regarding the weakness of the previous NRNA about not acheiving the mission along with the role and upcoming planning of NRNA with Kapil Poudel on Nagarik Dainiki
                     </p>
-                    <a href="https://www.facebook.com/Nayadainiki/videos/2819299258400747" class="btn btn-secondary">Read more...</a>
+                    <a href="https://www.facebook.com/Nayadainiki/videos/2819299258400747" class="btn btn-secondary" target="_blank">Read more...</a>
                 </div>
                 <div class="col-12 col-lg-4 item">
                     <img src="{{asset('front-assets/img/blog/blog2.jpg')}}" class="img-fluid" alt=""/>
@@ -28,15 +28,13 @@
                     <a href="#" class="btn btn-secondary">Read more...</a>
                 </div>
                 <div class="col-12 col-lg-4 item">
-                    <img src="{{asset('front-assets/img/blog/blog3.jpg')}}" class="img-fluid" alt=""/>
-                    <h4>Donec hendrerit ultricies orci eget pretium</h4>
+                    <img src="{{asset('front-assets/img/blog/blog3.png')}}" class="img-fluid" alt="corona-discussion"/>
+                    <h4>Corona discussion</h4>
 
                     <p>
-                        Vivamus a mattis augue. Suspendisse aliquam justo in nibh hendrerit lobortis. Proin euismod nisl
-                        sem, ac rhoncus ex lacinia nec.
-                        Aliquam bibendum vel dui eu tincidunt...
+                        What is Covid-19 and how South Korea is becoming success for the control of Covid? I have share my knowlege in the program organized by Nepal Research and Education Network.
                     </p>
-                    <a href="#" class="btn btn-secondary">Read more...</a>
+                    <a href="https://www.facebook.com/156218744417668/videos/915074156011822/" class="btn btn-secondary" target="_blank">Read more...</a>
                 </div>
             </div>
         </div>
