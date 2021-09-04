55a0d7ced6ca430566a4964ded7cfc50f65b5d6e
Manojgaire1 <gairemanoj2020@gmail.com>
2021-09-04 14:33:46 +0545

    blog new addition

--- resources/views/frontend/inc/blog.blade.php
+++ resources/views/frontend/inc/blog.blade.php
@@ -17,15 +17,13 @@
                     <a href="https://www.facebook.com/Nayadainiki/videos/2819299258400747" class="btn btn-secondary" target="_blank">Read more...</a>
                 </div>
                 <div class="col-12 col-lg-4 item">
-                    <img src="{{asset('front-assets/img/blog/blog2.jpg')}}" class="img-fluid" alt=""/>
-                    <h4>Donec hendrerit ultricies orci eget pretium</h4>
+                    <img src="{{asset('front-assets/img/blog/blog2.png')}}" class="img-fluid" alt=""/>
+                    <h4>Present situtation of Nepalese in South Korea along with employemt opportunity and posibilities of reentry.</h4>
 
                     <p>
-                        Vivamus a mattis augue. Suspendisse aliquam justo in nibh hendrerit lobortis. Proin euismod nisl
-                        sem, ac rhoncus ex lacinia nec.
-                        Aliquam bibendum vel dui eu tincidunt...
+                        Disccussion regarding the oppurtunites of Nepalese people in South Korea on 'Samaya Sambad' with Sabina Karki.
                     </p>
-                    <a href="#" class="btn btn-secondary">Read more...</a>
+                    <a href="https://www.facebook.com/100237805005782/videos/621349325410984/" class="btn btn-secondary" target="_blank">Read more...</a>
                 </div>
                 <div class="col-12 col-lg-4 item">
                     <img src="{{asset('front-assets/img/blog/blog3.png')}}" class="img-fluid" alt="corona-discussion"/>
