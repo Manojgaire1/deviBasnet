c44d058ebbe9193b648b4225e3d79cd5f8437ee4
Manojgaire1 <gairemanoj2020@gmail.com>
2021-09-04 14:01:57 +0545

    added new blog plost

--- resources/views/frontend/inc/blog.blade.php
+++ resources/views/frontend/inc/blog.blade.php
@@ -8,15 +8,13 @@
         <div class="container">
             <div class="row">
                 <div class="col-12 col-lg-4 item">
-                    <img src="{{asset('front-assets/img/blog/blog1.jpg')}}" class="img-fluid" alt=""/>
-                    <h4>Donec hendrerit ultricies orci eget pretium</h4>
+                    <img src="{{asset('front-assets/img/blog/blog1.png')}}" class="img-fluid" alt=""/>
+                    <h4>Role, aim and upcoming planning of NRNA</h4>
 
                     <p>
-                        Vivamus a mattis augue. Suspendisse aliquam justo in nibh hendrerit lobortis. Proin euismod nisl
-                        sem, ac rhoncus ex lacinia nec.
-                        Aliquam bibendum vel dui eu tincidunt...
+                       My opinion regarding the weakness of the previous NRNA about not acheiving the mission along with the role and upcoming planning of NRNA with Kapil Poudel on Nagarik Dainiki
                     </p>
-                    <a href="#" class="btn btn-secondary">Read more...</a>
+                    <a href="https://www.facebook.com/Nayadainiki/videos/2819299258400747" class="btn btn-secondary">Read more...</a>
                 </div>
                 <div class="col-12 col-lg-4 item">
                     <img src="{{asset('front-assets/img/blog/blog2.jpg')}}" class="img-fluid" alt=""/>
