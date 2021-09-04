de2ba23f1bdbbcf9482e449251f1e657368382f2
Manojgaire1 <gairemanoj2020@gmail.com>
2021-09-04 15:17:39 +0545

    blog updated along with content

--- resources/views/frontend/inc/blog.blade.php
+++ resources/views/frontend/inc/blog.blade.php
@@ -12,12 +12,12 @@
                     <h4>Role, aim and upcoming planning of NRNA</h4>
 
                     <p>
-                       My opinion regarding the weakness of the previous NRNA about not acheiving the mission along with the role and upcoming planning of NRNA with Kapil Poudel on Nagarik Dainiki
+                       My opinion regarding the previous and future planning of NRNA with Kapil Poudel on Nagarik Dainiki.
                     </p>
                     <a href="https://www.facebook.com/Nayadainiki/videos/2819299258400747" class="btn btn-secondary" target="_blank">Read more...</a>
                 </div>
                 <div class="col-12 col-lg-4 item">
-                    <img src="{{asset('front-assets/img/blog/blog2.png')}}" class="img-fluid" alt=""/>
+                    <img src="{{asset('front-assets/img/blog/blog2.jpg')}}" class="img-fluid" alt=""/>
                     <h4>Present situtation of Nepalese in South Korea along with employemt opportunity and posibilities of reentry.</h4>
 
                     <p>
