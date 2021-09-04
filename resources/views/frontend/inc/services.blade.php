6a304300bfcf9335f06cb146688c08f7fbacc019
Ram Tako <ramtako@naphix.com.au>
2021-09-04 10:09:21 +0545

    icon replaced with image

--- resources/views/frontend/inc/services.blade.php
+++ resources/views/frontend/inc/services.blade.php
@@ -18,14 +18,17 @@
                     </div>
                     <div class="col-12 col-md-6 col-lg-3">
                         <div class="box">
-                            <div class="circle"><i class="fa fa-cogs"></i></div>
+                            <div class="circle">
+                                <img class="img-fluid" src="{{asset('front-assets/img/yoga icon with background.svg')}}" alt="">
+                            </div>
                             <h3>Yoga Medititation</h3>
                             <p>For the healthy body and mind yoga and meditation play an improtant role. Do regular yoga for healthier life.</p>
                         </div>
                     </div>
                     <div class="col-12 col-md-6 col-lg-3">
                         <div class="box">
-                            <div class="circle"><i class="fa fa-code"></i></div>
+                            <div class="circle">
+                                <img class="img-fluid" src="{{asset('front-assets/img/motivator icon with background.svg')}}" alt=""></div>
                             <h3>Motivator</h3>
 
                             <p>As Korea being the aborad country, students are really fustrated. I love to motivate the young minds.</p>
