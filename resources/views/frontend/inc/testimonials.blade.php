afefc353c54cde60a105cabd9686c874fa4517d4
Manojgaire1 <gairemanoj2020@gmail.com>
2021-09-05 21:37:05 +0545

    added the file for the blog and others

--- resources/views/frontend/inc/testimonials.blade.php
+++ resources/views/frontend/inc/testimonials.blade.php
@@ -2,7 +2,7 @@
  <section id="testimonials" data-stellar-background-ratio="0.5" data-stellar-vertical-offset="100">
         <div class="section-header">
             <div>
-                <h2>What my clients say</h2>
+                <h2>What peoples say</h2>
             </div>
         </div>
         <div class="container">
@@ -11,9 +11,10 @@
                     <div class="gallery-cell">
                         <div class="testimonial">
                             <img class="testimonial-avatar"
-                                 src="{{asset('front-assets/img/client.png')}}" alt="">
-                            <q class="testimonial-quote">We wish Devi Basnet (PHD) all the best for the coming election.</q>
-                            <span class="testimonial-author"></span>
+                                 src="{{asset('front-assets/img/client1.png')}}" alt="">
+                            <q class="testimonial-quote">Dr. Basnet’s message is clear: “the time to act is now, connect and build the minds globally to prosper the motherland, and the future is ours to take.”
+</br>I support and wish Dr. Basnet a great success in his future endeavors.</q>
+                            <span class="testimonial-author">Yam N. Timsina(PhD),Founder Yaaad LLC, USA</span>
                         </div>
                     </div>
                     <div class="gallery-cell">
