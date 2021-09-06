0b300d74802b0d9f4ac140270b4b39a7b5d4d5fa
Manojgaire1 <gairemanoj2020@gmail.com>
2021-09-06 19:39:54 +0545

    updated url also

--- resources/views/frontend/inc/hireMe.blade.php
+++ resources/views/frontend/inc/hireMe.blade.php
@@ -4,7 +4,7 @@
             <div class="col-12">
                 <h2 class="animation" data-animation="fadeInLeft">NRNA election 2021</h2>
                 <h4 class="animation" data-animation="fadeInLeft">Dear All, I have announced the candidacy for secretary of NRNA-ICC 2021-2023. Please do find the detail of my candidacy manifesto in the below link.</h4>
-                <a data-animation="fadeInRight" class="btn btn-primary animation" href="https://nrna.org" target="_blank">View my 
+                <a data-animation="fadeInRight" class="btn btn-primary animation" href="{{url('/uploads/Manifesto_Devi_For_2021-2023_NRNAICC_Secretary.pdf')}}" target="_blank">View my 
                 manifesto!</a>
             </div>
         </div>
