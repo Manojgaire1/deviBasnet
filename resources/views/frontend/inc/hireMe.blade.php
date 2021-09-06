e03ff7cffe045a93139f8b8b6bbc2f6246e97d45
Manojgaire1 <gairemanoj2020@gmail.com>
2021-09-06 19:37:58 +0545

    update hire me section

--- resources/views/frontend/inc/hireMe.blade.php
+++ resources/views/frontend/inc/hireMe.blade.php
@@ -3,9 +3,9 @@
         <div class="section-header container">
             <div class="col-12">
                 <h2 class="animation" data-animation="fadeInLeft">NRNA election 2021</h2>
-                <h4 class="animation" data-animation="fadeInLeft">Preparing for election</h4>
-                <a data-animation="fadeInRight" class="btn btn-primary animation" href="https://nrna.org" target="_blank">View 
-                    more!</a>
+                <h4 class="animation" data-animation="fadeInLeft">Dear All, I have announced the candidacy for secretary of NRNA-ICC 2021-2023. Please do find the detail of my candidacy manifesto in the below link.</h4>
+                <a data-animation="fadeInRight" class="btn btn-primary animation" href="https://nrna.org" target="_blank">View my 
+                manifesto!</a>
             </div>
         </div>
     </section>
