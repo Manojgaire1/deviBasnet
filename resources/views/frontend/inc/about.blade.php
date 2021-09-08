c2ccccb34cafe88578b6b470ed682ba2089b8738
Manojgaire1 <gairemanoj2020@gmail.com>
2021-09-08 21:17:44 +0545

    added link for linkedin and the gate need to fix css

--- resources/views/frontend/inc/about.blade.php
+++ resources/views/frontend/inc/about.blade.php
@@ -10,9 +10,10 @@
                         <div class="photo-overlay">
                             <div class="photo-text social">
                                 <a href="https://www.facebook.com/drdevibasnet" target="_blank"><i class="fa fa-facebook"></i></a>
-                                <a href="https://www.linkedin.com/in/basnetdevi/"><i class="fa fa-linkedin"></i></a>
+                                <a href="https://www.linkedin.com/in/basnetdevi/" target="_blank"><i class="fa fa-linkedin"></i></a>
                                 <a href="https://twitter.com/basnetdevi" target="_blank"><i class="fa fa-twitter"></i></a>
                                 <!-- <a href="#"><i class="fa fa-tumblr"></i></a> -->
+                                <a href="https://www.researchgate.net/profile/Devi-Basnet-2" target="_blank"><img src="{{asset('front-assets/img/gate.svg')}}" class="img-fluid social_gate_icon"></a>
                                 <a href="https://www.instagram.com/drdevibasnet/" target="_blank"><i class="fa fa-instagram"></i></a>
                                 <!-- <a href="#"><i class="fa fa-youtube"></i></a> -->
                             </div>
