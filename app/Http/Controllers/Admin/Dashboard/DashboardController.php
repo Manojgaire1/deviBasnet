e83898d095962c1e7ab8d1f77761dd775bfccc69
Manojgaire1 <gairemanoj2020@gmail.com>
2021-09-09 16:55:39 +0545

    removed the dashboard things

--- app/Http/Controllers/Admin/Dashboard/DashboardController.php
+++ app/Http/Controllers/Admin/Dashboard/DashboardController.php
@@ -4,11 +4,34 @@ namespace App\Http\Controllers\Admin\Dashboard;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Blog;
+use App\Models\Activity;
+use App\Models\Timeline;
+use App\Models\Type;
+
 
 class DashboardController extends Controller
 {
     //
+    protected $totalActivity        = 0;
+    protected $totalBlogs           = 0;
+    protected $totalActivityType    = 0;
+    protected $totalTimeline        = 0;
+
+    /**
+     * function to show the list page
+     * @param void
+     */
     public function index(){
-        return view('admin.dashboard.index');
+        $this->totalActivity        = Activity::where('status','1')->get()->count();
+        $this->totalBlogs           = Blog::where('status','1')->get()->count();
+        $this->totalActivityType    = Type::where('status','1')->get()->count();
+        $this->totalTimeline        = Timeline::where('status','1')->get()->count();
+        return view('admin.dashboard.index')->with([
+            'totalActivity'       => $this->totalActivity,
+            'totalBlogs'          => $this->totalBlogs,
+            'totalActivityType'   => $this->totalActivityType,
+            'totalTimeline'       => $this->totalTimeline,
+        ]);
     }
 }
