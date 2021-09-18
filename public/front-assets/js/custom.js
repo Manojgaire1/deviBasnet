564bfef91baf1eade071cf5b82f1b89fe1256805
Manojgaire1 <gairemanoj2020@gmail.com>
2021-09-18 18:45:53 +0545

    remove the map error in console

--- public/front-assets/js/custom.js
+++ public/front-assets/js/custom.js
@@ -395,48 +395,48 @@
     /** GOOGLE MAP                                                                                                    */
     /******************************************************************************************************************/
 
-    window.myMap = function () {
-        // Create a new StyledMapType object, passing it an array of styles,
-        // and the name to be displayed on the map type control.
-        var styledMapType = new google.maps.StyledMapType(
-            [
-                {
-                    "featureType": "all",
-                    "elementType": "all",
-                    "stylers": [
-                        {
-                            "saturation": -100
-                        },
-                        {
-                            "gamma": 0.5
-                        }
-                    ]
-                }
-            ]);
+    //window.myMap = function () {
+    //     // Create a new StyledMapType object, passing it an array of styles,
+    //     // and the name to be displayed on the map type control.
+    //     var styledMapType = new google.maps.StyledMapType(
+    //         [
+    //             {
+    //                 "featureType": "all",
+    //                 "elementType": "all",
+    //                 "stylers": [
+    //                     {
+    //                         "saturation": -100
+    //                     },
+    //                     {
+    //                         "gamma": 0.5
+    //                     }
+    //                 ]
+    //             }
+    //         ]);
 
-        // Create a map object, and include the MapTypeId to add
-        // to the map type control.
-        var map = new google.maps.Map(document.getElementById('googleMap'), {
-            center: {lat: config_mapLat, lng: config_mapLng},
-            zoom: config_mapZoom,
-			scrollwheel: false,
-            mapTypeControlOptions: {
-                mapTypeIds: ['roadmap', 'satellite', 'hybrid', 'terrain',
-                    'styled_map']
-            }
-        });
+    //     // Create a map object, and include the MapTypeId to add
+    //     // to the map type control.
+    //     var map = new google.maps.Map(document.getElementById('googleMap'), {
+    //         center: {lat: config_mapLat, lng: config_mapLng},
+    //         zoom: config_mapZoom,
+	// 		scrollwheel: false,
+    //         mapTypeControlOptions: {
+    //             mapTypeIds: ['roadmap', 'satellite', 'hybrid', 'terrain',
+    //                 'styled_map']
+    //         }
+    //     });
 		
-        var marker = new google.maps.Marker({
-            position: new google.maps.LatLng(config_mapLat, config_mapLng),
-            map: map,
-            title: config_mapTitle,
-			icon: 'img/map-marker.png'
-        });
+    //     var marker = new google.maps.Marker({
+    //         position: new google.maps.LatLng(config_mapLat, config_mapLng),
+    //         map: map,
+    //         title: config_mapTitle,
+	// 		icon: 'img/map-marker.png'
+    //     });
 
-        //Associate the styled map with the MapTypeId and set it to display.
-        map.mapTypes.set('styled_map', styledMapType);
-        map.setMapTypeId('styled_map');
-    };
+    //     //Associate the styled map with the MapTypeId and set it to display.
+    //     map.mapTypes.set('styled_map', styledMapType);
+    //     map.setMapTypeId('styled_map');
+    // };
 
     /******************************************************************************************************************/
     /** PRELOADER                                                                                                     */
